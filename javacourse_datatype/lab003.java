e8967aeff577245b352c5f48e84595f3e11859cf
Deeshu Deol <nitindeolnd4@gmail.com>
2025-01-11 22:40:40 +0530

    Add files via upload
    continue learning

---
+++ javacourse_datatype/lab003.java
@@ -0,0 +1,23 @@
+package javacourse_datatype;
+
+public class lab003 {
+    public static void main(String[] args) {
+        // primitive data type
+
+        byte b = 100; //8 bits , 1byte
+        short c = 128; //16 bits, 2 byte
+        int d = 1000; //32 bits, 4 bytes
+        long e = 807770166L; // 64 bits, 8 bytes
+        char grade = 'A'; // a, b , c, d, e > ASCII value - int - assigned
+        // 16 bits, 2 byte
+        float f = 3.14f; //32 bits, 4 bytes
+        double g = 118.2345654d; //64 bits, 8 bytes
+
+        // Non primitive data type
+        String name = "Purna"; // String - bunch of char (collection of char)
+        // Array, Class - User Data type
+        // Interface
+
+
+    }
+}
